4a8485c06bc1a20328a82fdf2992f01924c317a2
Daniel Wagner-Hall <dawagner@gmail.com>
2026-04-22 17:17:56 +0100

    Comment out second

--- individual-shell-tools/awk/script-02.sh
+++ individual-shell-tools/awk/script-02.sh
@@ -6,4 +6,4 @@ set -euo pipefail
 # Your output should contain 6 lines, each with two words on it, separated by a space.
 awk '{print $1, $2}' scores-table.txt
 # Or alternatively, if we need more control over the formatting:
-awk '{printf "%s %s\n", $1, $2}' scores-table.txt
+#awk '{printf "%s %s\n", $1, $2}' scores-table.txt
